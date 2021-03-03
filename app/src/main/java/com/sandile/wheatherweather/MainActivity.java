8ec6ec3add9a954b901bc11696a18b176dd5b997
19013083 <69584864+19013083@users.noreply.github.com>
2021-03-03 03:03:52 +0200

    search layout + logic

--- app/src/main/java/com/sandile/wheatherweather/MainActivity.java
+++ app/src/main/java/com/sandile/wheatherweather/MainActivity.java
@@ -3,12 +3,48 @@ package com.sandile.wheatherweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_citysearch;
+    private TextView tv_cityname;
+    private EditText et_cityname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_citysearch = findViewById(R.id.main_btn_search);
+        btn_citysearch.setOnClickListener(this);
+
+        tv_cityname = findViewById(R.id.main_tv_cityname);
+        et_cityname = findViewById(R.id.main_et_citysearch);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.main_btn_search:
+                SearchCity();
+//            case R.id.main_btn_daily_weight:
+//                startActivity(new Intent(this, WeightLog.class));
+//                break;
+//            case R.id.main_btn_meal_log:
+//                startActivity(new Intent(this, MealLog.class));
+                break;
+        }
+    }
+
+    private void SearchCity() {
+        String CityName = et_cityname.getText().toString().trim();
+        tv_cityname.setText(et_cityname.getText());
+    }
+
+
 }
