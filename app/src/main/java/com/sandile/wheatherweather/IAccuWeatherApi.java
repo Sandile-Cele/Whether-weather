78bf5c97d5e0d89d5be4299d47cd3b86c9211267
Sandile-Cele <Sandilecele101@gmail.com>
2021-03-15 02:29:54 +0200

    !!!can search city

--- app/src/main/java/com/sandile/wheatherweather/IAccuWeatherApi.java
+++ app/src/main/java/com/sandile/wheatherweather/IAccuWeatherApi.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 //Find out why this is an interface??
 public interface IAccuWeatherApi {//Responsible for returning call object
@@ -17,7 +17,7 @@ public interface IAccuWeatherApi {//Responsible for returning call object
     Call<RootAccu5Day> getDurban5Day();//Tip name this what you are getting back
     //This is the Call object is of type "RootAccu5Day". And this the root of 5day forecast json from Accuweather
 
-    @GET("locations/v1/cities/search?apikey=xkiA0fqNBMqMLSc2dHK8Q8aIwGjWnBAY&q=Durban")
-    Call<List<RootAccuCitySearch>> getCitySearch();//@Path("qCitySearch") String citySearchText
+    @GET("locations/v1/cities/search?apikey=xkiA0fqNBMqMLSc2dHK8Q8aIwGjWnBAY")
+    Call<List<RootAccuCitySearch>> getCitySearch(@Query("q") String citySearchText);
     // ^ ^ ^  This call object is type "list" because the json is of type list. TIP because json starts with "[".
 }
--- app/src/main/java/com/sandile/wheatherweather/MainActivity.java
+++ app/src/main/java/com/sandile/wheatherweather/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -65,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private String getTextFromTv() {
+    private String AccuCityTextBuilder() {
         return et_cityname.getText().toString().trim();
     }
 
@@ -129,8 +130,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void SearchCityApi() {
-//        String tempCitySearchText = sdf
-        Call<List<RootAccuCitySearch>> onAccuCitySearch = oneIAccuWeatherApi.getCitySearch();
+        final String tempCitySearchText = AccuCityTextBuilder();
+        Log.d("city_search", tempCitySearchText);
+        Call<List<RootAccuCitySearch>> onAccuCitySearch = oneIAccuWeatherApi.getCitySearch(tempCitySearchText);
 
         onAccuCitySearch.enqueue(new Callback<List<RootAccuCitySearch>>() {
             @Override
@@ -163,38 +165,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    private void getForecast() {
-        Call<List<RootAccuCitySearch>> call = oneIAccuWeatherApi.getCitySearch();//"Call" is retrofit class
-        //call.enqueue is a thread running in the background, it's made by retrofit. It Needs to be in the background because it's a network operation and if it freezes the whole app does too???
+//    private void getForecast() {
+//        Call<List<RootAccuCitySearch>> call = oneIAccuWeatherApi.getCitySearch();//"Call" is retrofit class
+//        //call.enqueue is a thread running in the background, it's made by retrofit. It Needs to be in the background because it's a network operation and if it freezes the whole app does too???
+//
+//        call.enqueue(new Callback<List<RootAccuCitySearch>>() {
+//            @Override
+//            public void onResponse(Call<List<RootAccuCitySearch>> call, Response<List<RootAccuCitySearch>> response) {
+//                pb_cityDetails.setVisibility(View.GONE);
+//
+//                //response.isSuccessful ??? means response code is 200 to 300.
+//                if(!response.isSuccessful()){//If not successful then... This needs to be done because it could successfully respond 404 which is not good.
+//                    tv_cityname.setText("response.isSuccessful: \n " + response.code());
+//                    return;//Don't continue because data will be null.
+//                }
+//
+//                //Tip: if you type response.body you will see that it return of type "List<RootAccuCitySearch>"
+//                List<RootAccuCitySearch> AccuDetailsObj = response.body();
+//
+//                //how loop works: for("Data type you receive e.g int, string" -space- "temp object name": "source") {}
+//                for(RootAccuCitySearch oneCity: AccuDetailsObj){
+//                    tv_cityname.append("City rank: " + oneCity.getRank());
+//                    tv_cityname.append("\n\n City key: " +oneCity.getKey());
+//                    tv_cityname.append("\n\n City name: " +oneCity.getEnglishName());
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<RootAccuCitySearch>> call, Throwable t) {
+//                pb_cityDetails.setVisibility(View.GONE);
+//                tv_cityname.setText("OnFailure: \n" + t.getMessage());
+//            }
+//        });
+//    }
 
-        call.enqueue(new Callback<List<RootAccuCitySearch>>() {
-            @Override
-            public void onResponse(Call<List<RootAccuCitySearch>> call, Response<List<RootAccuCitySearch>> response) {
-                pb_cityDetails.setVisibility(View.GONE);
 
-                //response.isSuccessful ??? means response code is 200 to 300.
-                if(!response.isSuccessful()){//If not successful then... This needs to be done because it could successfully respond 404 which is not good.
-                    tv_cityname.setText("response.isSuccessful: \n " + response.code());
-                    return;//Don't continue because data will be null.
-                }
-
-                //Tip: if you type response.body you will see that it return of type "List<RootAccuCitySearch>"
-                List<RootAccuCitySearch> AccuDetailsObj = response.body();
-
-                //how loop works: for("Data type you receive e.g int, string" -space- "temp object name": "source") {}
-                for(RootAccuCitySearch oneCity: AccuDetailsObj){
-                    tv_cityname.append("City rank: " + oneCity.getRank());
-                    tv_cityname.append("\n\n City key: " +oneCity.getKey());
-                    tv_cityname.append("\n\n City name: " +oneCity.getEnglishName());
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<List<RootAccuCitySearch>> call, Throwable t) {
-                pb_cityDetails.setVisibility(View.GONE);
-                tv_cityname.setText("OnFailure: \n" + t.getMessage());
-            }
-        });
-    }
 }
