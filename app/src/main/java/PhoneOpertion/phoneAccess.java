279265061de41054c1d11064a69005e74a1711d3
Sandile-Cele <Sandilecele101@gmail.com>
2021-04-06 00:39:55 +0200

    broken screenshot

---
+++ app/src/main/java/PhoneOpertion/phoneAccess.java
@@ -0,0 +1,5 @@
+package PhoneOpertion;
+
+public class phoneAccess {
+
+}
--- app/src/main/java/com/sandile/wheatherweather/MainActivity.java
+++ app/src/main/java/com/sandile/wheatherweather/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -13,6 +14,7 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ProgressBar pb_cityDetails;
     private IAccuWeatherApi oneIAccuWeatherApi;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSION_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,6 +78,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         //When user starts app ask them to enable location!
+
+
+//        val builder: StrictMode.VmPolicy.Builder = StrictMode.VmPolicy.Builder()// This code is to remove data sharing restrictions
+//        StrictMode.setVmPolicy(builder.build())
+
+
     }
 
     @Override
@@ -86,31 +97,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 setLocation();
                 break;
             case R.id.main_fab_screenshot:
-                takeScreenshot();
+                takeScreenshotLogic();
                 break;
             case R.id.main_fab_gotoForecastList:
 //                finish();//This is to close activity
-                if(isThereCityKey())
+                if (isThereCityKey())
                     startActivity(new Intent(this, ForecastList.class));
                 break;
         }
     }
 
-    private Boolean isThereCityKey(){
-        if(ApiEngine.tempCityKey == 0){
-            Toast.makeText(this,"Search for city or get current location.",Toast.LENGTH_LONG).show();
+    private void takeScreenshotLogic() {
+        isStorageGranted(this);
+        takeScreenshot(getWindow().getDecorView().getRootView(), "screenshot");
+    }
+
+    private Boolean isThereCityKey() {
+        if (ApiEngine.tempCityKey == 0) {
+            Toast.makeText(this, "Search for city or get current location.", Toast.LENGTH_LONG).show();
             return Boolean.FALSE;
         }
         return Boolean.TRUE;
     }
 
-    private void setLocation(){
-        if(LocationManager.SetCurrentLocation(this)){
+    private void setLocation() {
+        if (LocationManager.SetCurrentLocation(this)) {
             LocationManager.restBool();
             AccuCitySearch(LocationManager.getLocationString(), new ApiEngine().RetrofitBuildBase());
-        }
-        else{
-            Toast.makeText(this,"try again",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "try again", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -123,19 +138,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void AccuCitySearch(String searchCityInput, IAccuWeatherApi oneIAccuObj) {//Static key set here
         pb_cityDetails.setVisibility(View.VISIBLE);
 
-        if(searchCityInput.isEmpty()){
-            Toast.makeText(this,"City input data is empty" + searchCityInput,Toast.LENGTH_LONG).show();
+        if (searchCityInput.isEmpty()) {
+            Toast.makeText(this, "City input data is empty" + searchCityInput, Toast.LENGTH_LONG).show();
             return;
         }
 
         Call<List<RootAccuCitySearch>> onAccuCitySearch = oneIAccuObj.getCitySearch(searchCityInput);
 
-        onAccuCitySearch.enqueue(new  Callback<List<RootAccuCitySearch>>() {
+        onAccuCitySearch.enqueue(new Callback<List<RootAccuCitySearch>>() {
             @Override
             public void onResponse(Call<List<RootAccuCitySearch>> call, Response<List<RootAccuCitySearch>> response) {
                 pb_cityDetails.setVisibility(View.GONE);
 
-                if(!response.isSuccessful()){
+                if (!response.isSuccessful()) {
                     tv_cityname.setText("response.isSuccessful: \n" + response.code());
                     pb_cityDetails.setVisibility(View.GONE);
                     return;
@@ -144,16 +159,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 List<RootAccuCitySearch> citySearchObj = response.body();
 
 
-                for(RootAccuCitySearch oneCity: citySearchObj){
+                for (RootAccuCitySearch oneCity : citySearchObj) {
                     ApiEngine.tempCityKey = oneCity.getKey();//Setting static key
 
                     String tempCityDetails = "";
 
                     tempCityDetails += "City rank: " + oneCity.getRank();
-                    tempCityDetails += "\nCity name: " +oneCity.getEnglishName();
-                    tempCityDetails += "\nCity key: " +oneCity.getKey();
-                    tempCityDetails += "\nType: " +oneCity.getType();
-                    tempCityDetails += "\nCity localized name: " +oneCity.getLocalizedName();
+                    tempCityDetails += "\nCity name: " + oneCity.getEnglishName();
+                    tempCityDetails += "\nCity key: " + oneCity.getKey();
+                    tempCityDetails += "\nType: " + oneCity.getType();
+                    tempCityDetails += "\nCity localized name: " + oneCity.getLocalizedName();
 
                     tv_cityname.setText(tempCityDetails);
                 }
@@ -167,45 +182,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    private void takeScreenshot() {
-        Date now = new Date();
-        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+    private File takeScreenshot(View view, String fileName) {
+        Date date = new Date();
+        CharSequence dateFormatted = DateFormat.format("yyyy-MM-dd_hh:mm:ss", date);
 
         try {
-            // image naming and path to include sd card appending name you choose for file
-//            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+            String imgDir = Environment.getExternalStorageDirectory().toString() + "/Whether-weather";
+            File fileDir = new File(imgDir);
+
+            if (!fileDir.exists()) {
+                if (!fileDir.mkdirs()) {
+                    Toast.makeText(this, "Could not make file to store images.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            String imgPath = imgDir + "/" + fileName + "-" + dateFormatted + ".jpeg";
 
             // create bitmap screen capture
-            View v1 = getWindow().getDecorView().getRootView();
-            v1.setDrawingCacheEnabled(true);
-            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
-            v1.setDrawingCacheEnabled(false);
+//            View v1 = getWindow().getDecorView().getRootView();
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
 
-//            File imageFile = new File(mPath);
+            File imageFile = new File(imgPath);
 
-//            FileOutputStream outputStream = new FileOutputStream(imageFile);
-//            int quality = 100;
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
-//            outputStream.flush();
-//            outputStream.close();
 
-        }
-        catch (Throwable e) {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileDir);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            return imageFile;
+        } catch (Throwable e) {
             // Several error may come out with file handling or DOM
             e.printStackTrace();
         }
+
+        return null;
     }
 
-    private void openScreenshot(File imageFile) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        Uri uri = Uri.fromFile(imageFile);
-        intent.setDataAndType(uri, "image/*");
-        startActivity(intent);
+    public static void isStorageGranted(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(activity, PERMISSION_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
     }
+
+    private void shareImageFromURI(Uri inImageToShare) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, inImageToShare);
+        shareIntent.setType("image/jpeg");
+//      startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.send_to)));
+        startActivity(Intent.createChooser(shareIntent, "Share using"));
+
+    }
+
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults){
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         //Check if user granted access to location
     }
 }
